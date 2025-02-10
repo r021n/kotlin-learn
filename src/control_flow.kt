05ca78f2913843200a3e3cd7cb28049863c11693
r021n <rozinabdul25@gmail.com>
2025-02-10 17:14:53 +0700

    initial commit

---
+++ src/control_flow.kt
@@ -0,0 +1,50 @@
+fun main() {
+
+//    Percabangan
+
+    println("\n===================\nif else percabangan\n===================")
+
+    val nilaiUjian: Int = 75
+
+    if (nilaiUjian > 70) {
+        println("selamat anda lulus")
+    } else {
+        println("maaf anda belum lulus")
+    }
+
+    val angka: Int = 0
+
+    if (angka > 0) {
+        println("angka positif")
+    } else if (angka < 0) {
+        println("angka negatif")
+    } else {
+        println("angka nol")
+    }
+
+//    Perulangan
+    println("\n===================\nloop biasa\n===================")
+
+    for (i in 1..5) {
+        println("iterasi ke -${i}")
+    }
+
+    println("\n===================\nloop dengan step\n===================")
+
+    for (i in 1..10 step 2){
+        println("lompatan ke -${i}")
+    }
+
+    println("\n===================\nloop for menurun\n===================")
+
+    for (i in 5 downTo 1) {
+        println("Turunan ke -${i}")
+    }
+
+    println("\n===================\nloop for collection\n===================")
+
+    val buahBuahan: List<String> = listOf("Apel", "Jeruk", "Mangga")
+    for (buah in buahBuahan) {
+        println("Buah ${buah}")
+    }
+}
---
+++ src/lesson_1.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var namaSaya: String = "nama saya budi"
+    val umur: Int = 30
+
+    println(namaSaya)
+    println(umur)
+}
