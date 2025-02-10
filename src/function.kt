7bd2f2ffc831582165cb9e1433d0d7d8a57fc7ad
r021n <rozinabdul25@gmail.com>
2025-02-10 23:47:59 +0700

    learn function

---
+++ src/function.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val hasilPenjumlahan: Int = tambah(5, 3)
+    println(hasilPenjumlahan)
+}
+
+fun tambah(angka1: Int, angka2: Int): Int {
+    var total = angka1 + angka2
+    return total
+}
