03a60b0ebcdb16c70c8274a8f9e7d72550831ee9
r021n <rozinabdul25@gmail.com>
2025-02-14 16:33:52 +0700

    make adding book feature for bookstore cashier project

--- src/lesson_8_bookstore_cashier.kt
+++ src/lesson_8_bookstore_cashier.kt
@@ -1,6 +1,28 @@
 fun main() {
-    var daftarBuku: MutableList<MutableMap<String, Any>> = mutableListOf(
-        mutableMapOf("Judul" to "Buku 1", "Harga" to 100000, "Stok" to 5),
-        mutableMapOf("Judul" to "Buku 2", "Harga" to 200000, "Stok" to 5))
-    println(daftarBuku[1]["Judul"])
+    var daftarBuku: MutableList<MutableMap<String, Any>> = mutableListOf()
+
+    fun tambahBuku() {
+
+        print("Masukan nama buku: ")
+        val namaBuku = readLine()
+        print("Masukan harga buku: ")
+        val hargaBuku = readLine()
+        print("Masukan jumlah buku: ")
+        val jumlahBuku = readLine()
+
+        if (namaBuku != null && hargaBuku != null && jumlahBuku != null){
+            daftarBuku.add(mutableMapOf(
+                "Judul" to namaBuku,
+                "Harga" to hargaBuku,
+                "Stok" to jumlahBuku))
+            println("Berhasil menambahkan buku $namaBuku")
+        } else {
+            println("Input tidak valid")
+        }
+    }
+
+    tambahBuku()
+    for (buku in daftarBuku) {
+        print("${buku["Judul"]} | ${buku["Harga"]} | ${buku["Stok"]}")
+    }
 }
